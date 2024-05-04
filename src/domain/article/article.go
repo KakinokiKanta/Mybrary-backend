a6e2613a5a16ea389af3ca5b1121cf0fc708f2a8
KakinokiKanta <kanta12621@gmail.com>
2024-05-04 23:46:40 +0900

    wip: 記事ドメインのファクトリ関数を追加

--- src/domain/article/article.go
+++ src/domain/article/article.go
@@ -1,5 +1,7 @@
 package article
 
+import "github.com/oklog/ulid/v2"
+
 type Article struct {
 	id          string
 	userID      string
@@ -7,4 +9,26 @@ type Article struct {
 	title       string
 	description string
 	tags        []string
+}
+
+func NewArticle(
+	userID string,
+	url string,
+	title string,
+	description string,
+	tags []string,
+) (*Article, error) {
+	// ulidパッケージでULIDを生成し、string型に変換
+	id := ulid.Make().String()
+
+	// TODO: 各種項目のバリデーション
+
+	return &Article{
+		id: id,
+		userID: userID,
+		url: url,
+		title: title,
+		description: description,
+		tags: tags,
+	}, nil
 }
