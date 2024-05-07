ac3944a60c3c00dbfb44730d82c6d7a4a1955ca6
KakinokiKanta <kanta12621@gmail.com>
2024-05-08 02:18:08 +0900

    update: 記事モデルのファクトリ関数にバリデーションを追加

--- src/domain/article/article.go
+++ src/domain/article/article.go
@@ -1,6 +1,11 @@
 package article
 
-import "github.com/oklog/ulid/v2"
+import (
+	"errors"
+	"unicode/utf8"
+
+	"github.com/oklog/ulid/v2"
+)
 
 type Article struct {
 	id          string
@@ -21,7 +26,22 @@ func NewArticle(
 	// ulidパッケージでULIDを生成し、string型に変換
 	id := ulid.Make().String()
 
-	// TODO: 各種項目のバリデーション
+	// URLのバリデーション
+	if utf8.RuneCountInString(url) < urlLengthMin {
+		return nil, errors.New("url is an incorrect value")
+	}
+	// 記事タイトルのバリデーション
+	if utf8.RuneCountInString(title) < titleLengthMin || titleLengthMax < utf8.RuneCountInString(title) {
+		return nil, errors.New("title is an incorrect value")
+	}
+	// 記事詳細のバリデーション
+	if utf8.RuneCountInString(description) < descriptionLengthMin || descriptionLengthMax < utf8.RuneCountInString(description) {
+		return nil, errors.New("description is an incorrect value")
+	}
+	// 記事に付加するタグのバリデーション
+	if len(tags) < tagsLengthMin || tagsLengthMax < len(tags) {
+		return nil, errors.New("url is an incorrect value")
+	}
 
 	return &Article{
 		id: id,
@@ -38,3 +58,20 @@ type ArticleTags []ArticleTag
 type ArticleTag struct {
 	tagID string
 }
+
+const (
+	// URLの最小値
+	urlLengthMin = 1
+
+	// 記事タイトルの最小値/最大値
+	titleLengthMin = 1
+	titleLengthMax = 100
+
+	// 記事詳細の最小値/最大値
+	descriptionLengthMin = 1
+	descriptionLengthMax = 1000
+
+	// タグの最小個数/最大個数
+	tagsLengthMin = 0
+	tagsLengthMax = 5
+)
