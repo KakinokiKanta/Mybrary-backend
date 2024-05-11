68d8304e322ed8821610dc5442cc79856a4306fb
KakinokiKanta <kanta12621@gmail.com>
2024-05-12 01:23:58 +0900

    add: 記事ドメインに関するリポジトリのインターフェースを定義

--- src/domain/article/article_repository.go
+++ src/domain/article/article_repository.go
@@ -1,5 +1,9 @@
 package article
 
 type ArticleRepository interface {
-	Create()
+	Create(article Article) error
+	FindById(id string) (Article, error)
+	FindAll() ([]Article, error)
+	Update(article Article) error
+	Delete(article Article) error
 }
