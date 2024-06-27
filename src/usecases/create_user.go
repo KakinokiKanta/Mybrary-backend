e2eedb5d0ff5f61244329512f947fb78b0d50663
KakinokiKanta <kanta12621@gmail.com>
2024-06-28 02:37:53 +0900

    add: 新規ユーザ作成のユースケースに対するDTOを定義

---
+++ src/usecases/create_user.go
@@ -0,0 +1,17 @@
+package usecases
+
+import "github.com/KakinokiKanta/Mybrary-backend/domain"
+
+type CreateUserUsecase struct {
+	userRepo domain.UserRepository
+}
+
+type CreateUserInputDTO struct {
+	Name string `json:"name"`
+}
+
+type CreateUserOutputDTO struct {
+	Id domain.UserID `json:"id"`
+	Name string `json:"name"`
+	CreatedAt string `json:"created_at"`
+}
