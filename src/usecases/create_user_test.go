83410614e91ae43d0a514503ceeec959c9545a67
KakinokiKanta <kanta12621@gmail.com>
2024-06-30 02:13:18 +0900

    add: 新規ユーザ登録ユースケースのテストケースを追加

--- src/usecases/create_user_test.go
+++ src/usecases/create_user_test.go
@@ -1,16 +1,45 @@
 package usecases
 
 import (
+	"testing"
+
 	"github.com/KakinokiKanta/Mybrary-backend/domain"
 )
 
 type mockUserRepoStore struct {
 	domain.UserRepository
 
-	result domain.User
+	result *domain.User
 	err error
 }
 
 func (m mockUserRepoStore) Create(_ domain.User) (domain.User, error) {
-	return m.result, m.err
+	return *m.result, m.err
+}
+
+func TestCreateUserUsecase(t *testing.T) {
+	repositoryResult, repositoryErr := domain.NewUser("Test user")
+
+	tests := []struct {
+		testName string
+		repository domain.UserRepository
+		args CreateUserInputDTO
+		expected *CreateUserOutputDTO
+		expectedErr bool
+	}{
+		{
+			testName: "Create user successful",
+			repository: mockUserRepoStore{
+				result: repositoryResult,
+				err: repositoryErr,
+			},
+			args: CreateUserInputDTO{
+				Name: "Test user",
+			},
+			expected: &CreateUserOutputDTO{
+				Name: "Test user",
+			},
+			expectedErr: false,
+		},
+	}
 }
