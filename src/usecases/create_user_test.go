04383eaabb7ee208eb7075a5696bca2dcbcb9ec2
KakinokiKanta <kanta12621@gmail.com>
2024-06-30 01:49:02 +0900

    add: 新規ユーザ登録ユースケースのテスト用モックを追加

---
+++ src/usecases/create_user_test.go
@@ -0,0 +1,16 @@
+package usecases
+
+import (
+	"github.com/KakinokiKanta/Mybrary-backend/domain"
+)
+
+type mockUserRepoStore struct {
+	domain.UserRepository
+
+	result domain.User
+	err error
+}
+
+func (m mockUserRepoStore) Create(_ domain.User) (domain.User, error) {
+	return m.result, m.err
+}
