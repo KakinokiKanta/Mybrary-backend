5087eea8c362af73a47fd2cea37ade5a868931da
KakinokiKanta <kanta12621@gmail.com>
2024-07-04 03:48:49 +0900

    adapter層のrepositoryにドメインリポジトリのインターフェースを満たす構造体を定義

--- src/adapter/repository/user_repository.go
+++ src/adapter/repository/user_repository.go
@@ -3,3 +3,8 @@ package repository
 // TODO: ここにデータベース処理を実装して
 // データベースを操作するExec処理等は
 // databaseに移行する
+type UserRepository struct {}
+
+func NewUserRepository() UserRepository {
+	return UserRepository{}
+}
