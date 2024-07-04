2a3b8c487edbc263c3afc3d6c2e8c35233d17996
KakinokiKanta <kanta12621@gmail.com>
2024-07-05 00:19:17 +0900

    add: Ginのルーティング関数に新規ユーザ登録のエントリポイントを追加

--- src/api/router/setup_router.go
+++ src/api/router/setup_router.go
@@ -1,12 +1,24 @@
 package router
 
 import (
+	"database/sql"
+
+	"github.com/KakinokiKanta/Mybrary-backend/api/controller"
+	"github.com/KakinokiKanta/Mybrary-backend/infrastracture/repository"
+	"github.com/KakinokiKanta/Mybrary-backend/usecase"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 )
 
-func setupRouter(db *gorm.DB) *gin.Engine {
+func setupRouter(db *sql.DB) *gin.Engine {
+	userRepository := repository.NewUserRepository(db)
+	createUserUsecase := usecase.NewCreateUserUsecase(userRepository)
+	createUserController := controller.NewCreateUserController(*createUserUsecase)
+
 	r := gin.Default()
+	r.Use(cors.Default())
+	userRouter := r.Group("/user")
+	userRouter.POST("", createUserController.Execute)
 
 	return r
 }
