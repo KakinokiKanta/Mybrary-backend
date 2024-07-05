1b9981ef441f8b84e6028cf706363d7ba4877b41
KakinokiKanta <kanta12621@gmail.com>
2024-07-06 03:02:31 +0900

    disable: とりあえずエラーを起こすinsert文はコメントアウト

--- _scripts/mysql/insertUser.sql
+++ _scripts/mysql/insertUser.sql
@@ -1,3 +1,3 @@
-# ユーザデータ2つ追加
-INSERT INTO users (id, name, created_at) values ("0123456789ABCDEFGHJKMNPQRSTVWXYZ", "User Name", now())
-INSERT INTO users (id, name, created_at) values ("jflkfjaweffhidusawbfdoixx", "user", now())
+-- # ユーザデータ2つ追加
+-- INSERT INTO users (id, name, created_at) values ("0123456789ABCD", "User Name", now())
+-- INSERT INTO users (id, name, created_at) values ("jflkfjaweffh", "user", now())
