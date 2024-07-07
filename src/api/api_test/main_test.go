c1c82cfc9f5eeb75e67c5a045cf887469499b4dc
KakinokiKanta <kanta12621@gmail.com>
2024-07-08 04:26:21 +0900

    add: APIテストの前処理でルータを起動する処理を追加

--- src/api/api_test/main_test.go
+++ src/api/api_test/main_test.go
@@ -4,15 +4,17 @@ import (
 	"database/sql"
 	"testing"
 
+	"github.com/KakinokiKanta/Mybrary-backend/api/router"
 	"github.com/KakinokiKanta/Mybrary-backend/infrastracture"
 )
 
-// リポジトリテスト全体で共有するsql.DB型
+// APIテスト全体で共有するsql.DB型
 var testDB *sql.DB
 
 func TestMain(m *testing.M)  {
 	// 前処理
 	testDB = infrastracture.SetupTest()
+	router.SetupRouter(testDB)
 
 	// パッケージ内のユニットテストをすべて実行
 	m.Run()
