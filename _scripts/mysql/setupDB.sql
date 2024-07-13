ec1f08eb549f3c40d281683dfe2b4832ce488047
KakinokiKanta <kanta12621@gmail.com>
2024-07-13 23:15:26 +0900

    fix: sqlファイルの修正

--- _scripts/mysql/setupDB.sql
+++ _scripts/mysql/setupDB.sql
@@ -2,6 +2,8 @@
 CREATE TABLE users (
   id VARCHAR(128) NOT NULL PRIMARY KEY,
   name VARCHAR(64) NOT NULL,
+  email VARCHAR(64) NOT NULL,
+  password VARCHAR(128) NOT NULL,
   created_at DATETIME,
   updated_at DATETIME
 )
