f30bd066319525fc11cff44e377beb037fd539cd
KakinokiKanta <kanta12621@gmail.com>
2024-07-16 03:41:12 +0900

    update: DB用のクエリに依存する形でFindByEmailを実装

--- src/infrastracture/repository/user_repository.go
+++ src/infrastracture/repository/user_repository.go
@@ -21,7 +21,8 @@ func (repo UserRepository) Create(user domain.User) (domain.User, error) {
 	var query = `
 		INSERT INTO users (id, name, email, password, created_at) VALUES (?, ?, ?, ?, ?);
 	`
-	// sql.DB型のメソッドExecを用いて、クエリを実行
+
+	// クエリを実行し、userドメインのフィールドを追加
 	_, err := repo.db.Exec(query, user.ID(), user.Name(), user.Email(), user.Password(), user.CreatedAt())
 	if err != nil {
 		return domain.User{}, err
@@ -30,9 +31,36 @@ func (repo UserRepository) Create(user domain.User) (domain.User, error) {
 	return user, nil
 }
 
-func (repo UserRepository) FindByEmail(user domain.User) (*domain.User, error) {
+func (repo UserRepository) FindByEmail(email string) (*domain.User, error) {
 	// usersテーブルからemailフィールドが一致するカラムを取得するクエリ
 	var query = `
 		SELECT * FROM users WHERE email = ?;
 	`
+
+	// emailが一致したカラムを取得
+	row := repo.db.QueryRow(query, email)
+	if err := row.Err(); err != nil {
+		return nil, err
+	}
+
+	var user DBUser
+	var createdAt sql.NullTime
+	var updatedAt sql.NullTime
+
+	// 取得したカラムから、DB用userモデルの各フィールドに値をスキャン
+	err := row.Scan(&user.id, &user.name, &user.email, &user.password, &createdAt, &updatedAt)
+	if err != nil {
+		return nil, err
+	}
+
+	// created_atフィールドがnullでないならば
+	if createdAt.Valid {
+		user.createdAt = createdAt.Time
+	}
+	// updated_atフィールドがnullでないならば
+	if updatedAt.Valid {
+		user.updatedAt = updatedAt.Time
+	}
+
+	return 
 }
