c6eab199fc31e10153d4ef4d25920e62b3c6ee35
KakinokiKanta <kanta12621@gmail.com>
2024-07-22 01:24:11 +0900

    update: login.go

--- src/usecase/login.go
+++ src/usecase/login.go
@@ -13,10 +13,6 @@ type LoginInputDTO struct {
 	Password string `json:"password" binding:"required,min=10,max=64"`
 }
 
-type LoginOutputDTO struct {
-	Token string `json:"token"`
-}
-
 func NewLoginUsecase(userRepo domain.UserRepository) *LoginUsecase {
 	return &LoginUsecase{
 		userRepo: userRepo,
@@ -28,7 +24,7 @@ func NewLoginUsecase(userRepo domain.UserRepository) *LoginUsecase {
  * これにより、トークン生成を行う外部パッケージに依存しない
  * また、認証手段を変更しやすくしている
  */
-func (uc LoginUsecase) Execute(input LoginInputDTO) (*LoginOutputDTO, error) {
+func (uc LoginUsecase) Execute(input LoginInputDTO) (bool, error) {
 	// TODO: DB内に同一メールアドレスが複数存在している場合のために、以下の処理をループさせる？
 	// メールアドレスでDB内を検索しユーザ情報を取得
 	user, err := uc.userRepo.FindByEmail(input.Email)
@@ -39,10 +35,5 @@ func (uc LoginUsecase) Execute(input LoginInputDTO) (*LoginOutputDTO, error) {
 	// JWTと取得したユーザ情報を基に認証処理
 	// usecase内にインターフェースを実装、中身はmiddlewareに
 
-	return &FindUserOutputDTO{
-		Id:       createdUser.ID(),
-		Name:     createdUser.Name(),
-		Email:    createdUser.Email(),
-		Password: createdUser.Password(),
-	}, nil
+	return input.Password == user.Password(), nil
 }
