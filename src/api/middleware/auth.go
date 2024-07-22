a63efee185be98232673995755daa073b004648c
KakinokiKanta <kanta12621@gmail.com>
2024-07-22 22:59:07 +0900

    update: auth.go

--- src/api/middleware/auth.go
+++ src/api/middleware/auth.go
@@ -8,42 +8,49 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func newJwtMiddleware() (*jwt.GinJWTMiddleware, error) {
+func NewJwtMiddleware(uc usecase.LoginUsecase) (*jwt.GinJWTMiddleware, error) {
 	jwtMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
-			Realm:      "mybrary authentication",
-			Key:        []byte("secret key"),
-			Timeout:    time.Hour * 24,
-			MaxRefresh: time.Hour * 24 * 7,
-			SendCookie: false,
-			PayloadFunc: func(data interface{}) jwt.MapClaims {
-					return jwt.MapClaims{
-							jwt.IdentityKey: data,
-					}
-			},
-			Authenticator: func(ctx *gin.Context) (interface{}, error) {
-					var input usecase.LoginInputDTO
+		Realm:      "mybrary authentication",
+		Key:        []byte("secret key"),
+		Timeout:    time.Hour * 24,
+		MaxRefresh: time.Hour * 24 * 7,
+		SendCookie: false,
+		PayloadFunc: func(data interface{}) jwt.MapClaims {
+			return jwt.MapClaims{
+				jwt.IdentityKey: data,
+			}
+		},
+		Authenticator: func(ctx *gin.Context) (interface{}, error) {
+			var input usecase.LoginInputDTO
 
-					if err := ctx.ShouldBind(&input); err != nil {
-							return "", jwt.ErrMissingLoginValues
-					}
+			if err := ctx.ShouldBind(&input); err != nil {
+				return "", jwt.ErrMissingLoginValues
+			}
 
-					if !l.isValid() {
-							return "", jwt.ErrFailedAuthentication
-					}
+			userID, isValid := uc.Execute(input)
+			if !isValid {
+				return "", jwt.ErrFailedAuthentication
+			}
 
-					return l.Email, nil
-			},
+			return userID, nil
+		},
 	})
 
 	if err != nil {
-			return nil, err
+		return nil, err
 	}
 
 	err = jwtMiddleware.MiddlewareInit()
 
 	if err != nil {
-			return nil, err
+		return nil, err
 	}
 
 	return jwtMiddleware, nil
+}
+
+func GetUserID(ctx *gin.Context) string {
+	claims := jwt.ExtractClaims(ctx)
+	userID := claims[jwt.IdentityKey]
+	return userID.(string)
 }
