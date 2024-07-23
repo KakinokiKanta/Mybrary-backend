843bcba1b6458a35bae1a97b1c8394f31123a1d7
KakinokiKanta <kanta12621@gmail.com>
2024-07-24 04:52:16 +0900

    wip: create_article.go (dtoのtagsをどうするか悩み中)

--- src/usecase/create_article.go
+++ src/usecase/create_article.go
@@ -6,16 +6,20 @@ type CreateArticleUseCase struct {
 	articleRepo articleDomain.ArticleRepository
 }
 
+type CreateArticleInputDto struct {
+	UserID      string `json:"user_id" binding:"required"`
+	Url         string `json:"url" binding:"required"`
+	Title       string `json:"title" binding:"required"`
+	Description string `json:"description"`
+	Tags        []articleDomain.ArticleTag `json: tags`
+}
+
+type ArticleTag struct {
+	TagName string `json:"tag_name" binding:"required"`
+}
+
 func NewCreateArticleUseCase (articleRepo articleDomain.ArticleRepository) *CreateArticleUseCase {
 	return &CreateArticleUseCase{
 		articleRepo: articleRepo,
 	}
-}
-
-type CreateArticleInputDto struct {
-	UserID      string
-	Url         string
-	Title       string
-	Description string
-	Tags        []articleDomain.ArticleTag
 }
