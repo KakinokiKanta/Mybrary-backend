7b4a65b288cd8b232f37c5309742d11fce76fd72
KakinokiKanta <kanta12621@gmail.com>
2024-07-29 04:58:30 +0900

    add: models.go dbTag

--- src/infrastracture/repository/models.go
+++ src/infrastracture/repository/models.go
@@ -16,3 +16,12 @@ type dbUser struct {
 	created_at sql.NullTime
 	updated_at sql.NullTime
 }
+
+type dbTag struct {
+	id         string
+	user_id    string
+	tag_name   string
+	used_num   string
+	created_at sql.NullTime
+	updated_at sql.NullTime
+}
