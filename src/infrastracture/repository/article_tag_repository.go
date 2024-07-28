ea4daa09aea85acee623a1cc89189f4252ccdf28
KakinokiKanta <kanta12621@gmail.com>
2024-07-29 04:59:05 +0900

    add: article_tag_repository.go FindByNameメソッド

--- src/infrastracture/repository/article_tag_repository.go
+++ src/infrastracture/repository/article_tag_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"strconv"
 
 	"github.com/KakinokiKanta/Mybrary-backend/domain"
 )
@@ -17,9 +18,9 @@ func NewArticleTagRepository(db *sql.DB) ArticleTagRepository {
 }
 
 func (repo ArticleTagRepository) Create(articleTag domain.ArticleTag) (domain.ArticleTag, error) {
-	// articleTagテーブルにデータを追加するクエリ
+	// tagsテーブルにデータを追加するクエリ
 	var query = `
-		INSERT INTO article_tags (id, user_id, tag_name, used_num) VALUES (?, ?, ?, ?);
+		INSERT INTO tags (id, user_id, tag_name, used_num) VALUES (?, ?, ?, ?);
 	`
 
 	// クエリを実行し、userドメインのフィールドを追加
@@ -29,4 +30,37 @@ func (repo ArticleTagRepository) Create(articleTag domain.ArticleTag) (domain.Ar
 	}
 
 	return articleTag, nil
+}
+
+func (repo ArticleTagRepository) FindByName(name string) (domain.ArticleTag, error) {
+	// tagsテーブルからemailフィールドが一致するカラムを取得するクエリ
+	var query = `
+		SELECT id, user_id, tag_name, used_num FROM tags WHERE tag_name = ?;
+	`
+
+	// nameが一致したカラムを取得
+	row := repo.db.QueryRow(query, name)
+	if err := row.Err(); err != nil {
+		return domain.ArticleTag{}, err
+	}
+
+	var dbTag dbTag
+
+	// 取得したカラムから、DB用tagモデルの各フィールドに値をスキャン
+	err := row.Scan(&dbTag.id, &dbTag.user_id, &dbTag.tag_name, &dbTag.used_num)
+	if err != nil {
+		return domain.ArticleTag{}, err
+	}
+
+	// DB用モデルからドメインモデルを生成
+	num, err := strconv.Atoi(dbTag.used_num)
+	if err != nil {
+		return domain.ArticleTag{}, err
+	}
+	articleTag, err := domain.ReArticleTag(domain.ArticleTagID(dbTag.id), domain.UserID(dbTag.user_id), dbTag.tag_name, num)
+	if err != nil {
+		return domain.ArticleTag{}, err
+	}
+
+	return *articleTag, nil
 }
