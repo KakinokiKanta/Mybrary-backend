68621728de8d7c8707f2e6fab583098a85e12473
KakinokiKanta <kanta12621@gmail.com>
2024-08-30 17:47:00 +0900

    fix

--- src/infrastracture/repository/main_test.go
+++ src/infrastracture/repository/main_test.go
@@ -4,21 +4,30 @@ import (
 	"database/sql"
 	"testing"
 
+	"github.com/KakinokiKanta/Mybrary-backend/domain"
 	"github.com/KakinokiKanta/Mybrary-backend/infrastracture"
 )
 
-// リポジトリテスト全体で共有するsql.DB型
+// リポジトリテスト全体で共有
 var testDB *sql.DB
+var succeedDomain *domain.ArticleTag
+const succeedTagName = "New Article Tag"
 
 func TestMain(m *testing.M)  {
 	// 前処理
-	testDB = infrastracture.SetupTest()
+	setup()
 
 	// パッケージ内のユニットテストをすべて実行
 	m.Run()
 
 	// 後処理
 	teardown()
+}
+
+// 前処理
+func setup() {
+	testDB = infrastracture.SetupTest()
+	succeedDomain, _ = domain.NewArticleTag("abcdefg1234AABBCCDD", succeedTagName)
 }
 
 // 接続したデータベースとのアクセスを閉じる後処理
