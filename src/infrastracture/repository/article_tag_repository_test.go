af6511ec3cd6c32da1620d1c520e3c135b95b03f
KakinokiKanta <kanta12621@gmail.com>
2024-09-01 21:49:58 +0900

    wip: article_tag_repository_test.goにUpdateNumメソッドのテスト追加中

--- src/infrastracture/repository/article_tag_repository_test.go
+++ src/infrastracture/repository/article_tag_repository_test.go
@@ -96,3 +96,49 @@ func TestFindArticleTagRepository(t *testing.T) {
 		})
 	}
 }
+
+func TestUpdateNumArticleTagRepository(t *testing.T) {
+	// TODO: 中身書き換えてないで
+	tests := []struct {
+		testName string
+		db *sql.DB
+		args string
+		expected domain.ArticleTag
+		expectedErr bool
+	}{
+		{
+			testName: "Successfully find article tag",
+			db: testDB,
+			args: succeedTagName,
+			expected: *succeedDomain,
+			expectedErr: false,
+		},
+		{
+			testName: "Failure: find article tag (this article tag does not exists in DB)",
+			db: testDB,
+			args: "Article Tag",
+			expected: domain.ArticleTag{},
+			expectedErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			repo := NewArticleTagRepository(testDB)
+
+			result, err := repo.FindByName(tt.args)
+			if (err != nil) != tt.expectedErr {
+				t.Errorf("[TestCase '%s'] Result: '%v' | ExpectedError: '%v'", tt.testName, err, tt.expectedErr)
+				t.Errorf("[TestCase '%s'] %s | %v | %v", tt.testName, tt.args, *succeedDomain, result)
+				return
+			}
+			diff := cmp.Diff(
+				result, tt.expected,
+				cmp.AllowUnexported(domain.ArticleTag{}),
+			)
+			if diff != "" {
+				t.Errorf("[TestCase '%s'] Result: '%v' | Expected: '%v'", tt.testName, result, tt.expected)
+			}
+		})
+	}
+}
